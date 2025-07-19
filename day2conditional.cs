1522912f4b4c016ce1ba50bd3215b76bb4b7d98d
charan1018 <kcharan1018@gmail.com>
2025-07-19 16:05:07 +0530

    Add files via upload

---
+++ day2conditional.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Conditional
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        
+        // Read input from user and convert to integer
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Check if number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"{number} is Even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is Odd.");
+        }
+    }
+}
